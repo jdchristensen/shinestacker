acbd6285555399b33f52aaa679e228edae5fed35
Luca Lista <luca.lista@infn.it>
2025-09-04 18:33:38 +0200

    update [ci skip]

--- src/shinestacker/algorithms/align_parallel.py
+++ src/shinestacker/algorithms/align_parallel.py
@@ -3,9 +3,11 @@ import copy
 import math
 import traceback
 from concurrent.futures import ThreadPoolExecutor, as_completed
+import cv2
 from ..config.constants import constants
 from .utils import read_img, img_subsample
-from .align import AlignFrames, detect_and_compute_matches
+from .align import (AlignFrames, detect_and_compute_matches, find_transform,
+                    check_affine_matrix, check_homography_distortion)
 
 
 class AlignFramesParallel(AlignFrames):
@@ -17,6 +19,7 @@ class AlignFramesParallel(AlignFrames):
         self._img_cache = None
         self._img_locks = None
         self._matches = None
+        self._transforms = None
 
     def cache_img(self, idx):
         self._img_locks[idx] += 1
@@ -30,7 +33,8 @@ class AlignFramesParallel(AlignFrames):
         self.sub_msg(f": preprocess {n_frames} images in parallel, {self.max_threads} cores")
         self._img_cache = [None] * n_frames
         self._img_locks = [0] * n_frames
-        self._matches = [(None, None, 0)] * n_frames
+        self._matches = [0] * n_frames
+        self._transforms = [None] * n_frames
         max_chunck_size = self.max_threads
         input_filepaths = self.process.input_filepaths()
 
@@ -55,9 +59,28 @@ class AlignFramesParallel(AlignFrames):
                 for future in as_completed(future_to_index):
                     idx = future_to_index[future]
                     try:
-                        kp_0, kp_ref, good_matches, message = future.result()
-                        self._matches[idx] = (kp_0, kp_ref, good_matches)
-                        self.sub_msg(f": found matches, image {idx}: {len(good_matches)} {message}")
+                        ret_message = future.result()
+                        message = f": found matches, image {idx}: {self._good_matches[idx]}"
+                        color=constants.LOG_COLOR_LEVEL_3
+                        if ret_message != '':
+                            message += f", {ret_message}"
+                            color=constants.LOG_COLOR_WARNING
+                        self.sub_msg(message, color=color)
+                        # if plot_path is not None:
+                        #    matches_mask = msk.ravel().tolist()
+                        #    img_match = cv2.cvtColor(cv2.drawMatches(
+                        #        img_8bit(img_0_sub), kp_0, img_8bit(img_ref_sub),
+                        #        kp_ref, good_matches, None, matchColor=(0, 255, 0),
+                        #        singlePointColor=None, matchesMask=matches_mask,
+                        #        flags=2), cv2.COLOR_BGR2RGB)
+                        #    plt.figure(figsize=constants.PLT_FIG_SIZE)
+                        #    plt.imshow(img_match, 'gray')
+                        #    save_plot(plot_path)
+                        #    if callbacks and 'save_plot' in callbacks:
+                        #        callbacks['save_plot'](plot_path)
+                        if ...:
+                            raise InvalidOptionError("transform", transform)
+
                     except Exception as e:
                         traceback.print_tb(e.__traceback__)
                         self.sub_msg(f": failed processing image: {idx}: {str(e)}")
@@ -93,7 +116,7 @@ class AlignFramesParallel(AlignFrames):
             subsample = int(1 + math.floor(img_res / target_res))
         fast_subsampling = self.alignment_config['fast_subsampling']
         min_good_matches = self.alignment_config['min_good_matches']
-        message = ''
+        messages = []
         while True:
             if subsample > 1:
                 img_0_sub = img_subsample(img_0, subsample, fast_subsampling)
@@ -106,5 +129,51 @@ class AlignFramesParallel(AlignFrames):
             if n_good_matches > min_good_matches or subsample == 1:
                 break
             subsample = 1
-            message = "no subsampling applied"
-        return kp_0, kp_ref, good_matches, message
+            messages.append("no subsampling applied")
+
+        m = None
+        min_matches = 4 if self.alignment_config['transform'] == constants.ALIGN_HOMOGRAPHY else 3
+
+        if n_good_matches >= min_matches:
+            transform = self.alignment_config['transform']
+            src_pts = np.float32([kp_0[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+            dst_pts = np.float32([kp_ref[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+            m, msk = find_transform(src_pts, dst_pts, transform, self.alignment_config['align_method'],
+                                    *(alignment_config[k]
+                                      for k in ['rans_threshold', 'max_iters',
+                                                'align_confidence', 'refine_iters']))
+
+            h_sub, w_sub = img_0_sub.shape[:2]
+            if subsample > 1:
+                if transform == constants.ALIGN_HOMOGRAPHY:
+                    low_size = np.float32([[0, 0], [0, h_sub], [w_sub, h_sub], [w_sub, 0]])
+                    high_size = np.float32([[0, 0], [0, h0], [w0, h0], [w0, 0]])
+                    scale_up = cv2.getPerspectiveTransform(low_size, high_size)
+                    scale_down = cv2.getPerspectiveTransform(high_size, low_size)
+                    m = scale_up @ m @ scale_down
+                elif transform == constants.ALIGN_RIGID:
+                    rotation = m[:2, :2]
+                    translation = m[:, 2]
+                    translation_fullres = translation * subsample
+                    m = np.empty((2, 3), dtype=np.float32)
+                    m[:2, :2] = rotation
+                    m[:, 2] = translation_fullres
+                else:
+                    messages.append("invalid transform")
+                    return None, None, None, ",".join(messages)
+
+            transform_type = self.alignment_config['transform']
+            is_valid = True
+            reason = ""
+            if transform_type == constants.ALIGN_RIGID:
+                is_valid, reason = check_affine_matrix(
+                    m, img_0.shape, affine_thresholds)
+            elif transform_type == constants.ALIGN_HOMOGRAPHY:
+                is_valid, reason = check_homography_distortion(
+                    m, img_0.shape, homography_thresholds)
+            if not is_valid:
+                messages.appen("invalid transform")
+                return None, None, None, ",".join(messages)
+        self._matches[idx] = len(good_matches)
+        self.__transforms[idx] = n
+        return ",".join(messages)
