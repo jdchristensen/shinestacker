a4b80bcf8352617c2704179933661d7d28b995a5
Luca Lista <luca.lista@infn.it>
2025-10-03 22:43:50 +0200

    improved contast correction [ci skip]

--- src/shinestacker/algorithms/corrections.py
+++ src/shinestacker/algorithms/corrections.py
@@ -11,19 +11,15 @@ def gamma_correction(img, gamma):
     return cv2.LUT(img, lut) if img.dtype == np.uint8 else np.take(lut, img)
 
 
-def contrast_correction(img, contrast):
+def contrast_correction(img, k):
     max_px_val = constants.MAX_UINT8 if img.dtype == np.uint8 else constants.MAX_UINT16
     ar = np.arange(0, max_px_val + 1, dtype=np.float64)
-    normalized = 2.0 * (ar / max_px_val) - 1.0
-    if contrast == 0:
-        corrected = normalized
-    else:
-        if contrast < 0:
-            corrected = np.sign(normalized) * \
-                (1.0 - (1.0 - np.abs(normalized)) ** (1.0 / (1.0 - contrast)))
-        else:
-            corrected = np.sign(normalized) * \
-                (1.0 - (1.0 - np.abs(normalized)) ** (1.0 + contrast))
+    x = 2.0 * (ar / max_px_val) - 1.0
+    # f(x) = x * exp(k) / (1 + (exp(k) - 1)|x|),  -1 < x < +1
+    exp_k = np.exp(k)
+    numerator = x * exp_k
+    denominator = 1 + (exp_k - 1) * np.abs(x)
+    corrected = numerator / denominator
     corrected = (corrected + 1.0) * 0.5 * max_px_val
-    lut = corrected.astype(img.dtype)
+    lut = np.clip(corrected, 0, max_px_val).astype(img.dtype)
     return cv2.LUT(img, lut) if img.dtype == np.uint8 else np.take(lut, img)
