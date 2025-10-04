2516cda91ec0aee2cc227a978379edc7f00e7f50
Luca Lista <luca.lista@infn.it>
2025-10-04 08:52:27 +0200

    code refactoring [ci skip]

--- src/shinestacker/retouch/lumi_contrast_filter.py
+++ src/shinestacker/retouch/lumi_contrast_filter.py
@@ -5,30 +5,35 @@ from .. algorithms.corrections import gamma_correction, contrast_correction
 
 
 class GammaSCurveFilter(BaseFilter):
-    def __init__(self, name, parent, image_viewer, layer_collection, undo_manager):
+    def __init__(
+                self, name, parent, image_viewer, layer_collection, undo_manager,
+                window_title, gamma_label, scurve_label):
         super().__init__(name, parent, image_viewer, layer_collection, undo_manager,
                          preview_at_startup=True)
-        self.min_lumi = -1
-        self.max_lumi = +1
-        self.initial_lumi = 0
-        self.min_contrast = -1
-        self.max_contrast = 1
-        self.initial_contrast = 0
+        self.window_title = window_title
+        self.gamma_label = gamma_label
+        self.scurve_label = scurve_label
+        self.min_gamma = -1
+        self.max_gamma = +1
+        self.initial_gamma = 0
+        self.min_scurve = -1
+        self.max_scurve = 1
+        self.initial_scurve = 0
         self.lumi_slider = None
         self.contrast_slider = None
 
     def setup_ui(self, dlg, layout, do_preview, restore_original, **kwargs):
-        dlg.setWindowTitle("Unsharp Mask")
+        dlg.setWindowTitle(self.window_title)
         dlg.setMinimumWidth(600)
         params = {
-            "Luminosity": (self.min_lumi, self.max_lumi, self.initial_lumi, "{:.1%}"),
-            "Contrast": (self.min_contrast, self.max_contrast, self.initial_contrast, "{:.1%}"),
+            self.gamma_label: (self.min_gamma, self.max_gamma, self.initial_gamma, "{:.1%}"),
+            self.scurve_label: (self.min_scurve, self.max_scurve, self.initial_scurve, "{:.1%}"),
         }
 
         def set_slider(name, slider):
-            if name == "Luminosity":
+            if name == self.gamma_label:
                 self.lumi_slider = slider
-            elif name == "Contrast":
+            elif name == self.scurve_label:
                 self.contrast_slider = slider
 
         value_labels = self.create_sliders(params, dlg, layout, set_slider)
@@ -41,18 +46,18 @@ class GammaSCurveFilter(BaseFilter):
 
         self.lumi_slider.valueChanged.connect(
             lambda v: update_value(
-                "Luminosity", v, self.min_lumi, self.max_lumi, params["Luminosity"][3]))
+                self.gamma_label, v, self.min_gamma, self.max_gamma, params[self.gamma_label][3]))
         self.contrast_slider.valueChanged.connect(
             lambda v: update_value(
-                "Contrast", v, self.min_contrast, self.max_contrast, params["Contrast"][3]))
+                self.scurve_label, v, self.min_scurve, self.max_scurve, params[self.scurve_label][3]))
         self.set_timer(do_preview, restore_original, dlg)
 
     def get_params(self):
         return (
             self.value_from_slider(
-                self.lumi_slider.value(), self.min_lumi, self.max_lumi),
+                self.lumi_slider.value(), self.min_gamma, self.max_gamma),
             self.value_from_slider(
-                self.contrast_slider.value(), self.min_contrast, self.max_contrast)
+                self.contrast_slider.value(), self.min_scurve, self.max_scurve)
         )
 
     def apply(self, image, lumi, contrast):
@@ -62,4 +67,7 @@ class GammaSCurveFilter(BaseFilter):
 
 
 class LumiContrastFilter(GammaSCurveFilter):
-    pass
+    def __init__(self, name, parent, image_viewer, layer_collection, undo_manager):
+        super().__init__(
+            name, parent, image_viewer, layer_collection, undo_manager,
+            "Luminosity, Contrast", "Luminosity", "Constrat")
