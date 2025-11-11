c8c20d4226793e679ccb4128539e020fc1a4d507
Luca Lista <luca.lista@infn.it>
2025-11-11 22:14:13 +0100

    fixed missing files [ci skip]

--- tests/test_0020_noise_detection.py
+++ tests/test_0020_noise_detection.py
@@ -1,6 +1,8 @@
+import os
 import matplotlib
 matplotlib.use('Agg')
 import logging
+from PIL import Image
 from shinestacker.config.constants import constants
 from shinestacker.core.logging import setup_logging
 from shinestacker.core.exceptions import ShapeError, BitDepthError
@@ -19,21 +21,49 @@ def check_fail_size(extension, directory, ExepctionType, files):
     assert shape_err
 
 
+def rm_dir(path):
+    for filename in os.listdir(path):
+        file_path = os.path.join(path, filename)
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+    os.rmdir(path)
+
+
 def test_detect_fail_1():
     setup_logging(
         console_level=logging.DEBUG,
         file_level=logging.DEBUG,
         log_file=f"logs/{constants.APP_STRING.lower()}.log"
     )
+    os.makedirs('output/img-jpg-wrong-size', exist_ok=True)
+    img1 = Image.new('RGB', (400, 600), color='black')
+    img2 = Image.new('RGB', (600, 400), color='black')
+    img1.save('output/img-jpg-wrong-size/image1.jpg', 'JPEG', quality=100)
+    img2.save('output/img-jpg-wrong-size/image2.jpg', 'JPEG', quality=100)
     check_fail_size("jpg", "img-jpg-wrong-size", ShapeError, (1, 2))
+    rm_dir('output/img-jpg-wrong-size')
 
 
 def test_detect_fail_2():
+    os.makedirs('output/img-tif-wrong-size', exist_ok=True)
+    img1 = Image.new('RGB', (400, 600), color='black')
+    img2 = Image.new('RGB', (600, 400), color='black')
+    img1_16bit = img1.convert('I;16')
+    img2_16bit = img2.convert('I;16')
+    img1_16bit.save('output/img-tif-wrong-size/image1.tif', 'TIFF')
+    img2_16bit.save('output/img-tif-wrong-size/image2.tif', 'TIFF')
     check_fail_size("tif", "img-tif-wrong-size", ShapeError, (1, 2))
+    rm_dir('output/img-tif-wrong-size')
 
 
 def test_detect_fail_3():
+    os.makedirs('output/img-tif-wrong-type', exist_ok=True)
+    img1 = Image.new('RGB', (400, 600), color='black')
+    img1.save('output/img-tif-wrong-type/image_8bit.tif', 'TIFF')
+    img1_16bit = img1.convert('I;16')
+    img1_16bit.save('output/img-tif-wrong-type/image_16bit.tif', 'TIFF')
     check_fail_size("tif", "img-tif-wrong-type", BitDepthError, ("_8bit", "_16bit"))
+    rm_dir('output/img-tif-wrong-type')
 
 
 def test_detect():
