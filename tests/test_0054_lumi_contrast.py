4125159efc3bca98ea1d615e0ba204d6347db5af
Luca Lista <luca.lista@infn.it>
2025-12-18 16:23:50 +0100

    more tests added [ci skip]

--- tests/test_0054_lumi_contrast.py
+++ tests/test_0054_lumi_contrast.py
@@ -19,6 +19,30 @@ def test_lumi_gamma():
 
 
 def test_contrast():
+    img = read_img("examples/input/img-jpg/0002.jpg")
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
+    try:
+        wb = contrast_correction(img, 1.2)
+        write_img(f"{out_path}/test-contrast.jpg", wb)
+        assert True
+    except Exception:
+        assert False
+
+
+def test_lumi_gamma_16():
+    img = read_img("examples/input/img-tif/0002.tif")
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
+    try:
+        wb = gamma_correction(img, 1.2)
+        write_img(f"{out_path}/test-gamma-lumi.tif", wb)
+        assert True
+    except Exception:
+        assert False
+
+
+def test_contrast_16():
     img = read_img("examples/input/img-tif/0002.tif")
     if not os.path.exists(out_path):
         os.makedirs(out_path)
@@ -33,3 +57,5 @@ def test_contrast():
 if __name__ == '__main__':
     test_lumi_gamma()
     test_contrast()
+    test_lumi_gamma_16()
+    test_contrast_16()
