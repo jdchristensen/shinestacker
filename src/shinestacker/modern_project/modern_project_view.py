e8ff39d6ddc99c8e9450fe8b35ad916df4e63529
Luca Lista <luca.lista@infn.it>
2025-12-23 19:48:59 +0100

    code refactoring [ci skip]

--- src/shinestacker/modern_project/modern_project_view.py
+++ src/shinestacker/modern_project/modern_project_view.py
@@ -57,10 +57,10 @@ class ModernProjectView(ProjectView):
     def refresh_ui(self):
         ProjectView.refresh_ui(self)
 
-    def refresh_and_set_status(self, status):
+    def refresh_and_set_status(self, _status):
         self.refresh_ui()
 
-    def refresh_and_select_job(self, job_idx):
+    def refresh_and_select_job(self, _job_idx):
         self.refresh_ui()
 
     def select_first_job(self):
