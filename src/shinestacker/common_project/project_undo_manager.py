8c0de3f4c372a08d8b47abc2bc35fa16fd6a696f
Luca Lista <luca.lista@infn.it>
2026-01-07 16:50:34 +0100

    support extra data [ci skip]

--- src/shinestacker/common_project/project_undo_manager.py
+++ src/shinestacker/common_project/project_undo_manager.py
@@ -45,3 +45,7 @@ class ProjectUndoManager(QObject):
     def reset(self):
         self._undo_buffer = []
         self.set_enabled_undo_action_requested.emit(False, '')
+
+    def add_extra_data_to_last_entry(self, label, data):
+        if len(self._undo_buffer) > 0:
+            self._undo_buffer[-1][label] = data
